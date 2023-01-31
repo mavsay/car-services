606201033e8a84ff1e1c315a08733697dff3e836
mavsay <aba@kodix.ru>
2023-01-31 10:44:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const cars = [
+  {
+    id: 'cars1',
+    name: 'WV',
+    models: ['POLO', 'JETTA', 'PASSAT'],
+  },
+  {
+    id: '',
+    name: 'WV',
+    model: 'POLO'
+  },
+  {
+    id: '',
+    name: 'WV',
+    model: 'POLO'
+  },
+  {
+    id: '',
+    name: 'WV',
+    model: 'POLO'
+  },
+];
+
+const works = [
+  {
+    id: '',
+    operation: '',
+  },
+  {
+    id: '',
+    operation: '',
+  },
+  {
+    id: '',
+    operation: '',
+  },
+  {
+    id: '',
+    operation: '',
+  }
+]
+
+const services = [
+  {
+    id: '',
+    service: 'TTS',
+    models: [
+      {
+        name: 'HONDA',
+        model: 'CIVIC',
+        works: [
+          {
+            operation: 'Замена масла',
+            price: 3000,
+          },
+          {
+            operation: 'Замена масла',
+            price: 3000,
+          }
+        ]
+      },
+      {
+        name: 'HONDA',
+        model: 'ACCORD',
+      }
+    ]
+  }
+];
+
+
+model 
+works
+
